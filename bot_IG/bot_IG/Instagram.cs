5283d0436c5b6a0c03e671d490bb1faffc232b0c
rafael <rafaelcpaes@unifei.edu.br>
2021-03-27 02:04:24 -0300

    c1 - adicao de classes Profile e Instagram

---
+++ bot_IG/bot_IG/Instagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bot_IG
+{
+    static public class Instagram // classe estatica = nao precisa ser instanciada para ser usada
+    {
+        public static Profile GetProfileByUser(string username)
+        {
+            
+        }
+    }
+}
