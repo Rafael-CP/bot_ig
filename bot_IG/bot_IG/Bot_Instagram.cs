ca426f0b81e501e8f2ad11fc9db24559d9dacee7
rafael <rafaelcpaes@unifei.edu.br>
2021-03-27 01:49:34 -0300

    c0 - inicio do projeto

---
+++ bot_IG/bot_IG/Bot_Instagram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bot_IG
+{
+    class Bot_Instagram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
